bbbdaf88c8a5fae9cc3572f594a81b5b8d17e882
Nafew Prodhan <nafew.prodhan@gmail.com>
2023-04-04 03:04:54 +0600

    Complete worldometer scrapping project using scrapy with datasets: json, xml, csv

--- worldometers/spiders/countries.py
+++ worldometers/spiders/countries.py
@@ -62,4 +62,44 @@ class CountriesSpider(scrapy.Spider):
             }
         
 
+# For data extraction -- command
+# scrapy crawl countries -o population_dataset.json
+# scrapy crawl countries -o population_dataset.csv
+# scrapy crawl countries -o population_dataset.xml           
+
+
+# clearer version of the template
+
+# import scrapy
+
+# class CountriesSpider(scrapy.Spider):
+#     name = "countries"
+#     allowed_domains = ["www.worldometers.info"]
+#     start_urls = ["https://www.worldometers.info/world-population/population-by-country/"]
+
+#     def parse(self, response):
+        
+#         countries = response.xpath("//td/a")
+
+#         for country in countries:
+#             name = country.xpath('.//text()').get()
+#             link = country.xpath('.//@href').get()
+
+#             yield response.follow(url=link, callback=self.parse_country, meta={'country_name':name})
             
+#     def parse_country(self, response):
+#         name = response.request.meta['country_name']
+#         rows = response.xpath("(//table[@class='table table-striped table-bordered table-hover table-condensed table-list'])[1]/tbody/tr")
+#         for row in rows:
+#             year = row.xpath(".//td[1]/text()").get()
+#             population = row.xpath(".//td[2]/strong/text()").get()
+#             yearly_changes = row.xpath(".//td[3]/text()").get()
+#             fertility_rate = row.xpath(".//td[7]/text()").get()
+
+#             yield {
+#                 "country_name": name,
+#                 "year": year,
+#                 "population": population,
+#                 "yearly_changes": yearly_changes,
+#                 "fertility_rate": fertility_rate
+#             }
